423bbf51971e30129713e5e229fae8b4305eb57d
Kevin Yeh <kevinyeh@alum.mit.edu>
2012-02-10 11:32:33 -0500

    parser

--- DocumentAnalyzer.java
+++ DocumentAnalyzer.java
@@ -11,20 +11,34 @@ package libreofficesandbox;
 public class DocumentAnalyzer {
     protected static java.util.regex.Pattern pHeader = java.util.regex.Pattern.compile("^[A-Z]+[/#-]?\\s??[A-Z]*:");
     protected java.util.ArrayList alHeaders = new java.util.ArrayList();
+    protected String mHeaderText="";
     public boolean ParseParagraph(com.sun.star.text.XTextContent xText)
     {
         String sParagraph=xText.getAnchor().getString().trim();
         //System.out.println(sParagraph);
         java.util.regex.Matcher mHeader = pHeader.matcher(sParagraph);
+
         if(mHeader.find())
         {
-            String mHeaderText=sParagraph.substring(mHeader.start(), mHeader.end()-1);
+            this.mHeaderText=sParagraph.substring(mHeader.start(), mHeader.end()-1);
             System.out.println(mHeaderText);
             alHeaders.add(mHeaderText);
+            if(sParagraph.length()>mHeader.end())
+            {
+                String sRestOfLine=sParagraph.substring(mHeader.end()+1).trim();
+                System.out.println(sRestOfLine);
+            }
         }
         else
         {
-        
+            if(sParagraph.length()==0)
+            {
+                this.mHeaderText="";
+            }
+            else
+            {
+                System.out.println(this.mHeaderText+":"+sParagraph);            
+            }
         }
         return true;
     }
